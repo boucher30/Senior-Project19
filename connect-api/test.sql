8b93efb38132c5884df18cb66cd3abdae5069120
stay-frosty1 <40503429+stay-frosty1@users.noreply.github.com>
2019-03-01 22:18:48 -0500

    Add files via upload
    added pwd field in user table. pwd for all users in table is abc.

--- connect-api/test.sql
+++ connect-api/test.sql
@@ -1,21 +1,22 @@
 create table user (
    user_id INT NOT NULL AUTO_INCREMENT,
    username VARCHAR(40) NOT NULL,
+   pwd VARCHAR(40) NOT NULL,
    first_name VARCHAR(20) NOT NULL,
    last_name VARCHAR(20) NOT NULL,
    PRIMARY KEY ( user_id )
 );
 
-INSERT INTO user (username, first_name, last_name)
-VALUES ('cmarcy', 'Christian', 'Marcy');
+INSERT INTO user (username, pwd, first_name, last_name)
+VALUES ('cmarcy', 'abc', 'Christian', 'Marcy');
 
-INSERT INTO user (username, first_name, last_name)
-VALUES ('amarcy', 'Anastasia', 'Marcy');
+INSERT INTO user (username, pwd, first_name, last_name)
+VALUES ('amarcy','abc', 'Anastasia', 'Marcy');
 
-INSERT INTO user (username, first_name, last_name)
-VALUES ('wmarcy', 'Wayne', 'Marcy');
+INSERT INTO user (username, pwd, first_name, last_name)
+VALUES ('wmarcy', 'abc', 'Wayne', 'Marcy');
 
-INSERT INTO user (username, first_name, last_name)
-VALUES ('emarcy', 'Emily', 'Marcy');
+INSERT INTO user (username, pwd, first_name, last_name)
+VALUES ('emarcy', 'abc', 'Emily', 'Marcy');
 
 SELECT * FROM user;
