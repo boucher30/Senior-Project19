a0e83c587ebc7df520ba5f021a3888748222d82d
Md Monir <47066686+monir1990@users.noreply.github.com>
2019-03-02 20:53:05 -0500

    Update test.sql
    I created buddyList table and able to display user's buddy list with buddy id.

--- connect-api/test.sql
+++ connect-api/test.sql
@@ -20,3 +20,35 @@ INSERT INTO user (username, pwd, first_name, last_name)
 VALUES ('emarcy', 'abc', 'Emily', 'Marcy');
 
 SELECT * FROM user;
+
+
+#buddyList table created
+create table buddyList (
+   user_id int not null,
+   user_id2 INT NOT NULL,
+   foreign key(user_id) references user(user_id))
+;
+
+
+#insert some sample buddy
+INSERT INTO buddyList (user_id,user_id2)
+VALUES ('1','2');
+
+INSERT INTO buddyList (user_id,user_id2)
+VALUES ('1','3');
+
+INSERT INTO buddyList (user_id,user_id2)
+VALUES ('1','4');
+INSERT INTO buddyList (user_id,user_id2)
+VALUES ('2','4');INSERT INTO buddyList (user_id,user_id2)
+VALUES ('3','2');
+
+#display buddyList table
+select * from buddyList;
+
+#this quory will return buddylist.
+select u.user_id2,us.first_name,us.last_name from
+(select user_id2 
+from buddyList
+where user_id = 1) as u join user us where u.user_id2 = us.user_id;
+
