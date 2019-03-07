01761de190baf58aa37e9e8e0d5be1c633cc1c71
Dylan Anderson <dylanjanderson4@gmail.com>
2019-03-07 14:06:32 -0500

    Made the file so that you dont have to use the .env file so we can show at the prototype

--- connect-api/db.js
+++ connect-api/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 // Connect to MySQL
 var con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME
+  host: "localhost",
+  user: "nodeuser",
+  password: "nodeuser@1234",
+	database: "test"
 });
 
 // Export connection so we dont open more connections
