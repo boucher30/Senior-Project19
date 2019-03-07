de598c72ec8fe90b926e5239bd09c57984f1079b
RJB <rboucher30@gmail.com>
2019-03-07 08:10:24 -0500

    venue profile with buttons

--- connect-react/src/components/VenuePage.js
+++ connect-react/src/components/VenuePage.js
@@ -5,39 +5,46 @@ import Col from 'react-bootstrap/Col';
 import Figure from 'react-bootstrap/Figure';
 import Mount_Snow from '../images/mount_snow_bg.png'
 import SideMenu from './SideMenu';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
 
 
 export default class VenuePage extends Component {
     render() {
         return (
             <>
-
                 <Row>
-                <SideMenu />
-                <Container>
-                    <Row>
-                        {/* <Col xs={2} style={{ paddingRight: '0px'}}>
-			                
-                        </Col> */}
-                        <Col>
-                            <Figure>
-                                <h1>Mount Snow Profile</h1>
-                                <Figure.Image
-                                    rounded
-                                    src={Mount_Snow}
-                                />
-                                <Figure.Caption style = {{color: 'black'}}>
-                                    Mount Snow is home to the East Coast's first All Park Mountain Face, Carinthia at Mount Snow, 
-                                    which debuted in the 2008-2009 season. Carinthia is home to nine parks, with both natural and 
-                                    man-made features and a half-pipe. Mount Snow was co-host of the first Extreme Games in 1995 
-                                    and host of the Winter X-Games in 2000 and 2001. Carinthia at Mount Snow claimed home to the 
-                                    second stop of the first annual Winter Dew Tour as well as many other events including the Freeski Open.
-                                </Figure.Caption>
-                            </Figure>
-                        </Col>
-                    </Row>
-                </Container>
+                    <SideMenu />
+                    <Container>
+                        <Row>
+                            <Col>
+                                <Figure>
+                                    <h1>Mount Snow Profile</h1>
+                                    <h4><em>Dover, VT</em></h4>
+                                    <Figure.Image
+                                        rounded
+                                        src={Mount_Snow}
+                                    />
+                                    <Figure.Caption style = {{color: 'black'}}>
+                                        Mount Snow is home to the East Coast's first All Park Mountain Face, Carinthia at Mount Snow, 
+                                        which debuted in the 2008-2009 season. Carinthia is home to nine parks, with both natural and 
+                                        man-made features and a half-pipe. Mount Snow was co-host of the first Extreme Games in 1995 
+                                        and host of the Winter X-Games in 2000 and 2001. Carinthia at Mount Snow claimed home to the 
+                                        second stop of the first annual Winter Dew Tour as well as many other events including the Freeski Open.
+                                    </Figure.Caption>
+                                </Figure>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Row>
+                <Row className = 'justify-content-center'>
+                    <ButtonGroup aria-label="Venue button group">
+                        <Button variant="secondary">Information</Button>
+                        <Button variant="secondary">Carves</Button>
+                        <Button variant="secondary">Media</Button>
+                    </ButtonGroup>
+                </Row>
+
             </>
         );
     }
