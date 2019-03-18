349a2f687ed24ffa7864cf98439010afc374379a
Christian Marcy <christian.marcy@gmail.com>
2019-03-18 17:38:08 -0400

    Added Edit Profile modal popup

--- connect-react/src/components/ProfilePage.js
+++ connect-react/src/components/ProfilePage.js
@@ -6,6 +6,7 @@ import Col from 'react-bootstrap/Col';
 import Container from "react-bootstrap/Container";
 import Image from 'react-bootstrap/Image';
 import SnowProfilePic from '../images/snowboard-profile-pic.jpg';
+import EditProfileModal from "./EditProfileModal";
 
 export default class ProfilePage extends Component {
 	constructor(props) {
@@ -14,15 +15,18 @@ export default class ProfilePage extends Component {
 			userId: props.match.params.number,
 			userInfo: {},
 			userInfoLength: 0,
-			isUserLoggedIn: props.match.params.number === localStorage.getItem('userId')
+			isUserLoggedIn: props.match.params.number === localStorage.getItem('userId'),
+			show: false
 		}
+
+		this.handleShow = this.handleShow.bind(this);
+		this.handleClose = this.handleClose.bind(this);
 	}
 
 	componentWillMount() {
 		// Getting the user id from the url param
 		axios.get(`http://localhost:8000/users/${this.state.userId}`)
 			.then(res => {
-				console.log('User:', res.data);
 				this.setState({
 					userInfo: res.data.user[0],
 					userInfoLength: Object.keys(res.data.user[0]).length
@@ -33,14 +37,27 @@ export default class ProfilePage extends Component {
 	// We need to conditionally render things based on the user in relation to who is logged in
 	render() {
 		if(this.state.userInfoLength > 0) {
-			const user = this.state.userInfo;
-			const profilePrefix = this.state.isUserLoggedIn ? 'My ' : `${this.state.userInfo.username}'s `
+			const { userInfo, isUserLoggedIn } = this.state;
+			const profilePrefix = isUserLoggedIn ? 'My ' : `${this.state.userInfo.username}'s `;
+
+			// Make button options for top right corner
+			let options;
+			if(isUserLoggedIn) {
+				options = <Button variant="warning" onClick={this.handleShow}>Edit</Button>;
+			} else {
+				options = <div style={{display:'flex'}}>
+					<Button style={{margin:'5px'}} variant="info">Follow</Button>
+					<Button style={{margin:'5px'}} variant="info">Add Buddy</Button>
+				</div>;
+			}
 
 			return (
 				<>
+					<EditProfileModal user={userInfo} show={this.state.show} handleClose={this.handleClose} />
+
 					<div style={{ display: 'flex', marginTop: '8px' }}>
-						<h2 style={{ width: '90%' }}>{profilePrefix} Profile</h2>
-						{this.state.isUserLoggedIn && <Button variant="warning">Edit</Button>}
+						<h2 style={{ width: isUserLoggedIn ? '90%' : '80%' }}>{profilePrefix} Profile</h2>
+						{options}
 					</div>
 
 					{/* This is the row that will hold the profile picture and the information */}
@@ -52,9 +69,9 @@ export default class ProfilePage extends Component {
 						</Col>
 						<Col xs={5}>
 							<Container style={{ paddingTop: '25%', paddingLeft: '60px' }}>
-								<p>Username: {user.username}</p>
-								<p>First Name: {user.first_name}</p>
-								<p>Last Name: {user.last_name}</p>
+								<p>Username: {userInfo.username}</p>
+								<p>First Name: {userInfo.first_name}</p>
+								<p>Last Name: {userInfo.last_name}</p>
 								<p>Buddy Count: {Math.round((Math.random() * (50)))}</p>
 							</Container>
 						</Col>
@@ -69,5 +86,13 @@ export default class ProfilePage extends Component {
 				<div>Loading! this is what we see</div>
 			);
 		}
+	}
+
+	handleClose() {
+		this.setState({ show: false });
+	}
+
+	handleShow() {
+		this.setState({ show: true });
 	}
 }
