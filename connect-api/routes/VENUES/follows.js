f9d27f9a1e0c60197071eea36a1477cc523e2a91
Dylans PC <dylanjanderson4@gmail.com>
2019-03-25 18:11:57 -0400

    updated venues carves.

--- connect-api/routes/VENUES/follows.js
+++ connect-api/routes/VENUES/follows.js
@@ -98,12 +98,13 @@ router.get('/:followId', (req,res) => {
 
 // updates follow
 router.put('/:followId', (req,res) => {
-	const followId = req.params.followId;
-	const {user1, user2, ven, ty} = req.body;
+	const followId = req.params.followId; //followId is the ID of the user
+	ven = req.params.venueId;
+	const {user1, ty} = req.body;
 	console.log("via put follow updated with id: " + followId);
-	update_follow = "CALL update_follow(?,?,?,?,?)";
+	update_follow = "CALL update_follow_venue(?,?,?,?)";
 
-	con.query(update_follow,[followId,user1, user2, ven, ty[0]],(err, results) => {
+	con.query(update_follow_venue,[followId, ven, ty[0]],(err, results) => {
 		if (err) throw err;
 		res.status(201).jsonp({msg:'follow updated via put',results}).end;
 	})
@@ -112,11 +113,12 @@ router.put('/:followId', (req,res) => {
 // updates all follows
 router.patch('/:followId', (req,res) => {
 	const followId = req.params.followId;
-	const {user1, user2, ven, ty} = req.body;
+	ven = req.params.venueId;
+	const {user1, ty} = req.body;
 	console.log(" via patch follow updated with id: " + followId);
-	update_follow = "CALL update_follow(?,?,?,?,?)";
+	update_follow = "CALL update_follow_venue(?,?,?,?)";
 
-	con.query(update_follow,[followId,user1, user2, ven, ty[0]],(err, results) => {
+	con.query(update_follow_venue,[followId, user1, ven, ty[0]],(err, results) => {
 		if (err) throw err;
 		res.status(201).jsonp({msg:'follow updated via patch',results}).end;
 	})
