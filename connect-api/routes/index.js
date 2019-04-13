372a20bd7a5d083f57c2e3e59a5bbf8b11b390d4
stay-frosty1 <40503429+stay-frosty1@users.noreply.github.com>
2019-04-13 17:10:03 -0400

    real time start express session cookie non functional. framework in play
    Signed-off-by: stay-frosty1 <40503429+stay-frosty1@users.noreply.github.com>

---
+++ connect-api/routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('login.ejs', { message: req.flash('loginMessage') });
+});
+
+router.get('/signup', function(req, res) {
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+});
+
+router.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile.ejs', { user: req.user });
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+module.exports = router;
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/');
+}
+
+...
+router.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true,
+}));
+
+router.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+    failureFlash: true,
+}));
+
+module.exports = router;
