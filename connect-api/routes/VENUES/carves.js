bfa2bf19f24e2582670ed0232f49a29460a34e39
mrsparkle <mrsparkle1114@gmail.com>
2019-04-15 12:39:58 -0400

    More commenting and code cleanup

--- connect-api/routes/VENUES/carves.js
+++ connect-api/routes/VENUES/carves.js
@@ -4,21 +4,16 @@ const con = require('../../db');
 
 
 
-// Grabs all carves from db
+// Grabs all carves from the database
 router.get('/', (req,res) => {
-    // Find all carves from database
-    console.log(req.params);
     venueId = req.params.venueId;
+    // Create a query to get all carves from the database
     carve_list = "CALL get_venue_carve(?)";
     // "CALL get_venue_carves(?)"   [venueId]
-
-    console.log(req.query);
-
+    // Execute the query
     con.query(carve_list,[venueId], (err, results) => {
         if (err) throw err;
-
         res.status(200).jsonp({results}).end;
-
     })
 });
 
@@ -27,114 +22,77 @@ router.get('/', (req,res) => {
 router.post('/', (req,res) => {
     const {carveName,creatorId,carveType,athlete,photo,date, sports} = req.body;
     venueId = req.params.venueId;
-    console.log(" new carve entered with carvename: " + carveName);
-    if(false)
-    {
-
-    }else{
-        // The carvename wasn't found in the database
-        // Create insert query for new carve
-        // Added a comment
-        new_carve = "CALL add_carve(?,?,?,?,?,?,?,?,?,?,?,?)";
-        // Execute the query to insert into the database
-        con.query(new_carve,[carveName,creatorId,venueId,carveType[0],athlete,photo,date, sports[0]], (err, results) => {
-            if (err) throw err;
-            res.status(201).jsonp({results}).end;
-        })
-
-    }
-});
-
-// updates all carves
-router.put('/', (req,res) => {
-
-    // The carvename wasn't found in the database
     // Create insert query for new carve
-    // Added a comment
-    new_carve = "CALL update_carves()";
+    new_carve = "CALL add_carve(?,?,?,?,?,?,?,?,?,?,?,?)";
     // Execute the query to insert into the database
-    con.query(new_carve,(err, results) => {
+    con.query(new_carve,[carveName,creatorId,venueId,carveType[0],athlete,photo,date, sports[0]], (err, results) => {
         if (err) throw err;
         res.status(201).jsonp({results}).end;
     })
 });
 
-// updates all carves
-router.patch('/', (req,res) => {
-
-    // The carvename wasn't found in the database
-    // Create insert query for new carve
-    // Added a comment
-    new_carve = "CALL update_carves()";
-    // Execute the query to insert into the database
-    con.query(new_carve,(err, results) => {
-        if (err) throw err;
-        res.status(201).jsonp({results}).end;
-    })
-
-
-});
-
-// deletes all carves
+// Delete all carves from the database
 router.delete('/', (req,res) => {
+    // Create query to delete all carves from the database
     delete_carves = "CALL delete_carves()";
+    //Execute the delete query
     con.query(delete_carves, (err, results) => {
         if (err) throw err;
         res.status(201).jsonp({results}).end;
     })
-
-
 });
 
-// Grab specific carve by their id
+// Grab specific carve by its id
 router.get('/:carveId', (req,res) => {
     const carveId = req.params.carveId;
+    // Create query to get the specified carve from the database
     get_carve  = "call get_carve(?)";
+    // Execute the query
     con.query(get_carve, [carveId],(err, results) => {
         if (err) throw err;
         res.status(200).jsonp({results}).end;
     })
 });
 
-// update carve by carve ID for given venue
+// Updates a single carve specified by the given carve ID
 router.put('/:carveId', (req,res) => {
     const carveId = req.params.carveId;
     venueId = req.params.venueId;
     const {carveName,creatorId,carveType,athlete,photo,date,completed, sports} = req.body;
-    console.log(" new carve updated with carvename: " + carveName);
+    // Create query to update the specified carve
     update_carve = "CALL update_carve(?,?,?,?,?,?,?,?,?,?,?,?,?)";
-
+    // Execute the update query
     con.query(update_carve,[carveId,carveName,creatorId,venueId,carveType[0],athlete,photo,date,completed, sports[0]],(err, results) => {
         if (err) throw err;
         res.status(201).jsonp({results}).end;
     })
 });
 
-// update carve by carve ID for given venue
+// Updates a single carve specified by the given carve ID
 router.patch('/:carveId', (req,res) => {
     const carveId = req.params.carveId;
     venueId = req.params.venueId;
     const {carveName,creatorId,carveType,athlete,photo,date,completed, sports} = req.body;
-    console.log(" new carve updated with carvename: " + carveName);
+    // Create query to update the specified carve
     update_carve = "CALL update_carve(?,?,?,?,?,?,?,?,?,?,?,?,?)";
-
+    // Execute the update query
     con.query(update_carve,[carveId,carveName,creatorId,venueId,carveType[0],athlete,photo,date,completed,sports[0]],(err, results) => {
         if (err) throw err;
         res.status(201).jsonp({results}).end;
     })
 });
 
-// deletes carve
+// Deletes a single carve specified by the given carve ID
 router.delete('/:carveId', (req,res) => {
     const carveId = req.params.carveId;
     console.log(" deleting carve with carve id: " + carveId);
+    // Create a query to delete the specified carve from the database
     delete_carves = "CALL delete_carve(?)";
+    // Execute the delete query
     con.query(delete_carves, [carveId],(err, results) => {
         if (err) throw err;
         res.status(201).jsonp({msg:'carve deleted'}).end;
     })
-
-
 });
 
 
--- connect-api/routes/VENUES/comments.js
+++ connect-api/routes/VENUES/comments.js
@@ -4,134 +4,89 @@ const con = require('../../db');
 
 
 
-// Grabs all comments from db
+// Grabs all comments for a specific venue from the database
 router.get('/', (req,res) => {
-    // Find all comments from database
+    // Create a query to get all comments from the database
     comment_list = "CALL get_comments()";
-
-
-    console.log(req.query);
-
+    // Execute the query
     con.query(comment_list, (err, results) => {
         if (err) throw err;
-
         res.status(200).jsonp({results}).end;
-
     })
 });
 
 
-// Creates a new comment
+// Creates a new comment and a specific venue
 router.post('/', (req,res) => {
     const {poster,carve,media,profile,comment} = req.body;
-
-    console.log(" new comment from: " + poster);
-    if(false)
-    {
-
-    }else{
-        // The commentname wasn't found in the database
         // Create insert query for new comment
-        // Added a comment
         new_comment = "CALL add_comment(?,?,?,?,?)";
         // Execute the query to insert into the database
         con.query(new_comment,[poster,carve,media,profile,comment], (err, results) => {
             if (err) throw err;
             res.status(201).jsonp({results}).end;
         })
-
-    }
 });
 
-// updates all comments
-router.put('/', (req,res) => {
-
-    // The commentname wasn't found in the database
-    // Create insert query for new comment
-    // Added a comment
-    new_comment = "CALL update_comments()";
-    // Execute the query to insert into the database
-    con.query(new_comment,(err, results) => {
-        if (err) throw err;
-        res.status(201).jsonp({results}).end;
-    })
-});
-
-// updates all comments
-router.patch('/', (req,res) => {
-
-    // The commentname wasn't found in the database
-    // Create insert query for new comment
-    // Added a comment
-    new_comment = "CALL update_comments()";
-    // Execute the query to insert into the database
-    con.query(new_comment,(err, results) => {
-        if (err) throw err;
-        res.status(201).jsonp({results}).end;
-    })
-
-
-});
-
-// deletes all comments
+// Deletes all comments for a specific venue
 router.delete('/', (req,res) => {
+    // Create the query to delete all comments from the database
     delete_comments = "CALL delete_comments()";
+    // Execute the delete query
     con.query(delete_comments, (err, results) => {
         if (err) throw err;
         res.status(201).jsonp({results}).end;
     })
-
-
 });
 
 // Grab specific comment by their id
 router.get('/:commentId', (req,res) => {
     const commentId = req.params.commentId;
-
+    // Create the query to get the specified comment from the database
     get_comment  = "call get_comment(?)";
+    // Execute the get query
     con.query(get_comment, [commentId],(err, results) => {
         if (err) throw err;
         res.status(200).jsonp({results}).end;
     })
 });
 
-// updates comment
-router.put('/:commentId', (req,res) => {
+// Updates the comment specified by the given comment ID
+    router.put('/:commentId', (req,res) => {
     const commentId = req.params.commentId;
     const {poster,carve,media,profile,comment} = req.body;
-    console.log("comment updated via put with commentId: " + commentId);
+    // Create the query to update the specified comment
     update_comment = "CALL update_comment(?,?,?,?,?,?)";
-
+    // Execute update query
     con.query(update_comment,[commentId,poster,carve,media,profile,comment],(err, results) => {
         if (err) throw err;
         res.status(201).jsonp({results}).end;
     })
 });
 
-// updates all comments
+// Updates the comment specified by the given comment ID
 router.patch('/:commentId', (req,res) => {
     const commentId = req.params.commentId;
     const {poster,carve,media,profile,comment} = req.body;
-    console.log("comment updated via patch with commentId: " + commentId);
+    // Create the query to update the specified comment
     update_comment = "CALL update_comment(?,?,?,?,?,?)";
-
+    // Execute update query
     con.query(update_comment,[commentId,poster,carve,media,profile,comment],(err, results) => {
         if (err) throw err;
         res.status(201).jsonp({results}).end;
     })
 });
 
-// deletes comment
+// Deletes the comment specified by the given comment ID
 router.delete('/:commentId', (req,res) => {
     const commentId = req.params.commentId;
-    console.log(" deleting comment with comment id: " + commentId);
+    // Create the query to delete the specified comment from the databse
     delete_comments = "CALL delete_comment(?)";
+    // Execute the delete query
     con.query(delete_comments, [commentId],(err, results) => {
         if (err) throw err;
         res.status(201).jsonp({msg:'comment deleted'}).end;
     })
-
-
 });
 
 
