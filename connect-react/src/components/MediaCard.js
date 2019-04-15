707a523b7187b4fe313c741f1d2c06129509dbf5
RJB <rboucher30@gmail.com>
2019-04-15 15:40:44 -0400

    maps every single media

--- connect-react/src/components/MediaCard.js
+++ connect-react/src/components/MediaCard.js
@@ -35,7 +35,7 @@ export default class MediaCard extends Component {
             axios.get(`http://localhost:8000/users/${this.props.profile_id}/media/`)
                 .then(res => {
                     this.setState({
-                        mediaInfo: res.data.results[0][0],
+                        mediaInfo: res.data.results[0],
                         mediaInfoLength: Object.keys(res.data.results[0][0]).length
                     });
                 });
@@ -157,75 +157,82 @@ export default class MediaCard extends Component {
 
 
     render() {
-        if(this.state.mediaInfoLength > 0){
-            const {mediaInfo} = this.state;
-            return (
-                <>
-                    <Card style = {{width: '35rem'}}>
-                            <container className="embed-responsive embed-responsive-16by9" style = {{ width: "100%", paddingBottom: "40px", border: '1px solid grey'}}>
-                                    <iframe title="User Media" className="embed-responsive-item"
-                                            src= {mediaInfo.url} allowFullScreen > </iframe>
-                                            
-                            </container>
-                        <Card.Body>
-                            <container>
-                                <Row style = {{marginTop: '-1rem', borderBottom:'1px dashed grey'}}>
-                                    <Card.Link href = "#">{mediaInfo.poster}</Card.Link>
-                                    :{mediaInfo.description}
-                                </Row>
-                                <Row style= {{marginTop: '1rem'}}>
-                                    
-                                    <FormGroup>
-                                        <Form.Row>
-                                            <Col className = "col-18">
-                                                <Form.Control size = "sm" type="text" placeholder="Say something interesting..." />
-                                            </Col>
-                                            <Col className = "col-1">
-                                                <Button size = "sm">Enter</Button>
-                                            </Col>
-                                        </Form.Row>
-                                    </FormGroup>
-                                </Row>
-                            </container>
-                        </Card.Body>
-                        <Table striped borderless hover size = "sm" style = {{marginTop: '-2rem'}}>
-                            <thead>
-                                <th>
-                                    username
-                                </th>
-                                <th>
-                                    comment
-                                </th>
-                                <th>
-                                    timestamp
-                                </th>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <Card.Link href = "#">CMarcy98</Card.Link>
-                                    <td>Wow this was so cool! I am testing word wrap aadf akjdf dfdfasdf sak df d a;akje fadkjfw</td>
-                                    <td className = "text-muted" style = {{fontSize: '13px'}}><em>3mins ago</em></td>
-                                </tr>
-                                <tr>
-                                    <Card.Link href = "#">frosty</Card.Link>
-                                    <td>I wish I could've made it.</td>
-                                    <td className = "text-muted" style = {{fontSize: '13px'}}><em>1 hour ago</em></td>
-                                </tr>
-                            </tbody>
+        let mediaList;
+        if(this.state.mediaInfo.length > 0){
+            mediaList = this.state.mediaInfo.map((media, index) => {
+                const {mediaInfo} = this.state;
+                return (
+                        <ListGroup.Item key={index} style={{
 
-                        </Table>
-                        <Card.Footer style = {{fontSize: '10px'}}><em>Create_Time{mediaInfo.time}</em></Card.Footer>
-                    </Card>
-
-                </>
-            )
-        }else {
-            return (
-                <div>
-                    <h1 className="fa fa-spinner fa-spin" style={{position: 'absolute', left: '50%', top: '50%'}}>Loading! </h1>
-                </div>
-            );
+                            fontFamily: 'monospace', paddingRight: '0px', paddingLeft: '0px',paddingTop: '0px',paddingBottom: '10px', width: "100%"
+                        }}>
+                        <Card style = {{width: '35rem'}}>
+                                <container className="embed-responsive embed-responsive-16by9" style = {{ width: "100%", paddingBottom: "40px", border: '1px solid grey'}}>
+                                        <iframe title="User Media" className="embed-responsive-item"
+                                                src= {media.url} allowFullScreen > </iframe>
+                                                
+                                </container>
+                            <Card.Body>
+                                <container>
+                                    <Row style = {{marginTop: '-1rem', borderBottom:'1px dashed grey'}}>
+                                        <Card.Link href = "#">{media.poster}</Card.Link>
+                                        :{media.description}
+                                    </Row>
+                                    <Row style= {{marginTop: '1rem'}}>
+                                        
+                                        <FormGroup>
+                                            <Form.Row>
+                                                <Col className = "col-18">
+                                                    <Form.Control size = "sm" type="text" placeholder="Say something interesting..." />
+                                                </Col>
+                                                <Col className = "col-1">
+                                                    <Button size = "sm">Enter</Button>
+                                                </Col>
+                                            </Form.Row>
+                                        </FormGroup>
+                                    </Row>
+                                </container>
+                            </Card.Body>
+                            <Table striped borderless hover size = "sm" style = {{marginTop: '-2rem'}}>
+                                <thead>
+                                    <th>
+                                        username
+                                    </th>
+                                    <th>
+                                        comment
+                                    </th>
+                                    <th>
+                                        timestamp
+                                    </th>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <Card.Link href = "#">CMarcy98</Card.Link>
+                                        <td>Wow this was so cool! I am testing word wrap aadf akjdf dfdfasdf sak df d a;akje fadkjfw</td>
+                                        <td className = "text-muted" style = {{fontSize: '13px'}}><em>3mins ago</em></td>
+                                    </tr>
+                                    <tr>
+                                        <Card.Link href = "#">frosty</Card.Link>
+                                        <td>I wish I could've made it.</td>
+                                        <td className = "text-muted" style = {{fontSize: '13px'}}><em>1 hour ago</em></td>
+                                    </tr>
+                                </tbody>
+    
+                            </Table>
+                            <Card.Footer style = {{fontSize: '10px'}}><em>Create_Time{media.time}</em></Card.Footer>
+                        </Card>
+                    </ListGroup.Item>
+                ) //return
+            });
         }
-     }
-}
-
+        return (
+            <>
+    
+                <ListGroup variant="flush" defaultActiveKey="1" style ={{paddingTop:"20px",width:"100%"}}>
+                    {mediaList}
+                </ListGroup>
+    
+            </>
+        )
+    }
+}//class
